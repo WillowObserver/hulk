3c905764a913f2e8c7cdc7afaeacd014dd530421
WillowObserver <38207982+WillowObserver@users.noreply.github.com>
2018-08-05 17:53:17 +0800

    update

--- src/main/java/com/here/hulk/HulkApplication.java
+++ src/main/java/com/here/hulk/HulkApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HulkApplication {
-
+	//1
 	public static void main(String[] args) {
 		SpringApplication.run(HulkApplication.class, args);
 	}
